e564b7f2cfb2e78953894e0a75547b3b1d6fc7ef
Albert Lo <lohoi@umich.edu>
2016-01-12 21:50:57 -0500

    Added in accuracy statistics and commented code

--- Zombies.py
+++ Zombies.py
@@ -13,10 +13,12 @@ IDLE = -1
 WIDTH = 700
 HEIGHT = 500
 
+
 class Projectile(pygame.sprite.Sprite):
     def __init__(self,position,fire_direction):
         super().__init__()
 
+        #Creates a fireball with the correct direction
         direct = 0
         direct = fire_direction
         self.dir = direct
@@ -37,7 +39,10 @@ class Projectile(pygame.sprite.Sprite):
             self.image = pygame.transform.scale(self.image, [25, 25])
             self.image.set_colorkey(WHITE)
             self.rect = self.image.get_rect()
-        
+
+            
+        # Moves the fireball slightly so that it doesn't
+        # fire from Player's head
         pos = [0.0 , 0.0]
         pos[0] = position[0] +7
         pos[1] = position[1] + 20
@@ -47,7 +52,9 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.y = pos[1]
         
     def update(self):
-        if self.dist == 170:
+        # updates fireball with a speed of 3
+        if self.dist == 140:
+            # kills the fireball when it reaches a certain distance
             self.kill()
         elif self.dir == 0:
            self.rect.y += 3
@@ -68,21 +75,19 @@ class Zombie(pygame.sprite.Sprite):
         super().__init__()
         pos = [0.0 , 0.0]
 
+        #spawns zombie in random location within map
         pos[0] = random.uniform(0.0,700)  # x
         pos[1] = random.uniform(0.0,500) # y
-        
+
+        #creates the zombie
         self.image = pygame.image.load("zombie1front.png").convert()
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
 
-        #self.player_pos[0] = Alby.rect.x
-        #self.player_pos[1] = Alby.rect.y
-
         self.speed = 1
-        
-    # end ctor
+    # end ctor()
 
     # loses precision if speed is not an integer
     def update(self,num):
@@ -94,10 +99,6 @@ class Zombie(pygame.sprite.Sprite):
             self.rect.y += self.speed
         if self.rect.y > num.y:
             self.rect.y -= self.speed
-
-        #print(type(self.rect.x))
-        #print(type(num.x))
-        
     #end update()
 #end Zombie()
 
@@ -106,6 +107,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.timer = 0
 
+    #spin is used for char select screen
     def spin(self,screen):
         if self.timer < 20:
             screen.blit(self.image,[250, 250])
@@ -127,7 +129,8 @@ class Albert(Player):
     def __init__(self):
         super().__init__()
         self.name = "Alby"
-        
+
+        #loads the 4 directions of Alby
         self.image = pygame.image.load("f_albwhite.png").convert()
         self.image.set_colorkey(WHITE)
         self.image = pygame.transform.scale(self.image, [50,50])
@@ -146,14 +149,9 @@ class Albert(Player):
         
         self.prev = 0
 
+        #Alby spawns at loc. x = 250, y = 250
         self.rect.x = 250
         self.rect.y = 250
-        print(type(self.rect.x))
-        '''
-        self.left_rect = self.pos
-        self.right_rect = self.pos
-        self.back_rect = self.pos
-        '''
         
         # -1 is a key to remember prev (for idle)
         # 0 for down
@@ -164,8 +162,10 @@ class Albert(Player):
     #end ctor()
 
     def update_pos(self,screen,keys_pressed):
+        #For movemen
         if keys_pressed[pygame.K_DOWN]:
             if self.rect.y + 2 < HEIGHT - 50:
+                # Conditions create boundaries for map
                 self.rect.y += 2.0
                 self.prev = 0
             screen.blit(self.image, self.rect)
@@ -185,6 +185,7 @@ class Albert(Player):
                 self.prev = 3
             screen.blit(self.char_left, self.rect)
         elif self.direction == IDLE:
+            #If no direction
             if self.prev == 0:
                 screen.blit(self.image, self.rect)
             elif self.prev == 1:
@@ -223,24 +224,32 @@ def main():
     Authorfont = pygame.font.SysFont('Calibri',30,False,True)
     Namefont = pygame.font.SysFont('Calibri', 20, False,False)
     Startfont = pygame.font.SysFont('Arial', 30, True, False)
-    
+
+    #background img
     background_img = pygame.image.load("zombie.png").convert()
+
+    #init the sprite groups
     Alby = Albert();
     all_sprites_list = pygame.sprite.Group()
     all_sprites_list.add(Alby)
-
     fire_list = pygame.sprite.Group()
     zombie_list = pygame.sprite.Group()
 
+    #Any variables for indexing
     indexer = 1
     time_indexer = 0
-    points = 0
+    
     end_scene = False
+
+    points = 0
+    num_fired = 0
      
     # -------- Main Program Loop -----------
     while not done:
         # --- Main event loop
         for event in pygame.event.get():
+            # extracts player actions from keyboard
+            # and stores within keys_pressed
             if event.type == pygame.QUIT:
                 done = True
                 break;
@@ -267,25 +276,14 @@ def main():
                     else:    
                         fire = Projectile(Alby.rect,Alby.direction)
                         fire_list.add(fire)
+                        num_fired += 1
 
-            keys_pressed = pygame.key.get_pressed()
-            
-            
+            keys_pressed = pygame.key.get_pressed()      
 
-        # --- Game logic should go here
-     
-        # --- Screen-clearing code goes here
-     
-        # Here, we clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
-     
-        # If you want a background image, replace this clear with blit'ing the
-        # background image.
-        
-     
-        # Intro Page
         if intro_trigger:
+            # Intro Page
             screen.blit(background_img, [0,0])
+            #flickr is used to control how fast the text flashes
             if flickr_count < 20:
                 start = Startfont.render("Hit Start to Play",True, WHITE) 
                 screen.blit(start,[75,400])
@@ -294,41 +292,57 @@ def main():
                 flickr_count +=1
                 if flickr_count > 30:
                     flickr_count = 0
+
+            # Title page text
             title = Titlefont.render("Zombie Game",True, BLACK)
             screen.blit(title, [60, 100])
-            
             authors = Authorfont.render("Created by Chris Quinones and Albert Lo", True, BLACK)
             illustrator = Authorfont.render("Player Sprite Art by Doug Wu", True,BLACK)
             screen.blit(authors, [60,150])
             screen.blit(illustrator, [60,175])
-
-        # Choose character screen
+            
         elif choose_character:
+            # Choose character screen
+
+            #Background color
             screen.fill(RED)
 
+            #text
             instruct = Authorfont.render("Instructions: ",True, BLACK)
             instructions = Namefont.render("Use the arrows keys to move and spacebar to fire projectiles. ",True,BLACK)
-            
+            start = Startfont.render("Hit Start to Play",True, WHITE) 
             name = Namefont.render(Alby.name, True, BLACK)
-            Alby.spin(screen)
+
+            #display text to screen
             screen.blit(instruct, [10,10])
             screen.blit(instructions, [10,40])
-
-            start = Startfont.render("Hit Start to Play",True, WHITE) 
-            screen.blit(start,[75,400])
-            
+            screen.blit(start,[400,400])
             screen.blit(name,[260, 305])
+
+            # spins the character
+            Alby.spin(screen)
             
-
-        # Regular gameplay
         elif(end_scene):
+            #end scene  
             screen.fill(RED)
-            point_print = Namefont.render("Number of zombies killed: " +str(points) ,True,BLACK)
-            screen.blit(point_print, [10, 10])
 
-            restart_print = Namefont.render("Hit enter to try again",True,BLACK)
-            screen.blit(restart_print, [50, 50])
+            #calculates accuracy
+            if num_fired == 0:
+                accuracy = 0.0
+            else:
+                accuracy = (points/num_fired) * 100
+            
+            accuracy = Namefont.render("Accuracy: " + str(accuracy) ,True,BLACK)
+            restart_print = Namefont.render("Hit enter to try again", True, WHITE)
+            point_print = Namefont.render("Number of zombies killed: " + str(points), True, BLACK)
+            lose_print = Titlefont.render("YOU LOSE!", True, BLACK)
 
+            screen.blit(lose_print, [250,200])
+            screen.blit(point_print, [250, 250])
+            screen.blit(accuracy, [250,270])
+            screen.blit(restart_print, [250, 300])
+
+            # Restarts the game
             if keys_pressed[pygame.K_RETURN]:
                 intro_trigger = True
                 choose_character = False
@@ -339,19 +353,22 @@ def main():
                 time_indexer = 0
                 points = 0
                 continue
-                
-            
         else:
+            # Regular gameplay
             if (time_indexer == 0):
+                # Creates zombies only when time_indexer is 0
                 for i in range(indexer):
                     test = Zombie()
                     while(abs(test.rect.x - Alby.rect.x) < 200 or abs(test.rect.y - Alby.rect.y) < 200):
+                        # If zombie spawns within 200 units of character
                         test.rect.x = random.uniform(0.0,700) # x
                         test.rect.y = random.uniform(0.0,500) # y
                     zombie_list.add(Zombie())
+                #increase the capacity of zombies spawned for next round
                 indexer += 1
             time_indexer += 1
             if(time_indexer > 200):
+                #time_indexer controls how much time is between zombie spawns
                 time_indexer = 0
             screen.fill(WHITE)
             Alby.update_pos(screen,keys_pressed)
@@ -359,16 +376,16 @@ def main():
             fire_list.draw(screen)
             zombie_list.update(Alby.rect)
             zombie_list.draw(screen)
-        
+
+            #kills zombies if they collide with projectiles
             hit_list = pygame.sprite.groupcollide(zombie_list, fire_list,True,True)
 
+            #increment points if zombie hit
             for i in hit_list:
                 points += 1
             
-            
             for zombie in zombie_list:
                 if Alby.rect.colliderect(zombie):
-                    print("YOU LOSE!")
                     end_scene = True
                     continue
      
